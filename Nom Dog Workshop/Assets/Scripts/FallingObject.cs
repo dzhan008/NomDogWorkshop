a1f8ddc52f9ac02fa14be32fa121e50e6f33575b
dzhan008 <dzhan008@ucr.edu>
2018-07-21 13:55:47 -0700

    Added Base Game

---
+++ Nom Dog Workshop/Assets/Scripts/FallingObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour {
+
+    public int TorqueForce = -100;
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Rigidbody2D>().AddTorque(TorqueForce);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Nom Dog Workshop/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Sprite IdleSprite;
+    public Sprite EatingSprite;
+    public Sprite CryingSprite;
+
+    public GameManager Manager;
+
+    bool eating = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Eat()
+    {
+        if(!eating)
+        {
+            eating = true;
+            gameObject.GetComponent<SpriteRenderer>().sprite = EatingSprite;
+        }
+    }
+
+    public void Idle()
+    {
+        if(eating)
+        {
+            eating = false;
+        }
+        gameObject.GetComponent<SpriteRenderer>().sprite = IdleSprite;
+    }
+
+    public void Cry()
+    {
+        eating = false;
+        gameObject.GetComponent<SpriteRenderer>().sprite = CryingSprite;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(eating)
+        {
+            if (collision.gameObject.tag == "Bone")
+            {
+                Manager.IncrementScore();
+            }
+            else if (collision.gameObject.tag == "Bomb")
+            {
+                Manager.GameEnd();
+            }
+            Destroy(collision.gameObject);
+        }
+    }
+}
